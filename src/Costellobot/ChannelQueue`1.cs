ace691e12fbdeb1adc1939a6adf0c2e0713f8b10
martincostello <martin@martincostello.com>
2022-04-02 09:41:09 +0100

    Reduce duplication
    Make an abstract class for the repeated Channel-based queue.

---
+++ src/Costellobot/ChannelQueue`1.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Threading.Channels;
+
+namespace MartinCostello.Costellobot;
+
+public abstract class ChannelQueue<T>
+{
+    private const int QueueCapacity = 1000;
+
+    private readonly Channel<T> _queue;
+
+    protected ChannelQueue()
+    {
+        var channelOptions = new BoundedChannelOptions(QueueCapacity)
+        {
+            FullMode = BoundedChannelFullMode.DropOldest,
+            SingleReader = true,
+            SingleWriter = false,
+        };
+
+        _queue = Channel.CreateBounded<T>(channelOptions);
+    }
+
+    protected Channel<T> Queue => _queue;
+
+    public async virtual Task<T?> DequeueAsync(CancellationToken cancellationToken)
+    {
+        T? item = default;
+
+        if (await _queue.Reader.WaitToReadAsync(cancellationToken))
+        {
+            item = await _queue.Reader.ReadAsync(cancellationToken);
+        }
+
+        return item;
+    }
+
+    public virtual bool Enqueue(T item)
+    {
+        return _queue.Writer.TryWrite(item);
+    }
+}
--- src/Costellobot/ClientLogQueue.cs
+++ src/Costellobot/ClientLogQueue.cs
@@ -1,42 +1,8 @@
 ﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using System.Threading.Channels;
-
 namespace MartinCostello.Costellobot;
 
-public sealed partial class ClientLogQueue
+public sealed partial class ClientLogQueue : ChannelQueue<ClientLogMessage>
 {
-    private const int QueueCapacity = 1000;
-
-    private readonly Channel<ClientLogMessage> _queue;
-
-    public ClientLogQueue()
-    {
-        var channelOptions = new BoundedChannelOptions(QueueCapacity)
-        {
-            FullMode = BoundedChannelFullMode.DropOldest,
-            SingleReader = true,
-            SingleWriter = false,
-        };
-
-        _queue = Channel.CreateBounded<ClientLogMessage>(channelOptions);
-    }
-
-    public async Task<ClientLogMessage?> DequeueAsync(CancellationToken cancellationToken)
-    {
-        ClientLogMessage? logEntry = null;
-
-        if (await _queue.Reader.WaitToReadAsync(cancellationToken))
-        {
-            logEntry = await _queue.Reader.ReadAsync(cancellationToken);
-        }
-
-        return logEntry;
-    }
-
-    public void Enqueue(ClientLogMessage logEntry)
-    {
-        _ = _queue.Writer.TryWrite(logEntry);
-    }
 }
