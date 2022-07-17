276f52c7f7eb4bd7e55e9f257693d8f898d89869
martincostello <martin@martincostello.com>
2022-07-17 11:45:44 +0100

    Add Jest
    - Add Jest for TypeScript tests.
    - Only compile the TypeScript once.
    - Remove redundant files from publish output.

---
+++ src/Costellobot/scripts/ts/App.test.ts
@@ -0,0 +1,11 @@
+// Copyright (c) Martin Costello, 2022. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+import { describe, expect, test } from '@jest/globals';
+import { App } from './App';
+
+describe('App', () => {
+    test('should be defined', () => {
+        expect(App).toBeDefined();
+    });
+});
