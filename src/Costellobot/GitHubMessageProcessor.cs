308fe4e07f39b720cdf047307992c5e2efdcfd50
martincostello <martin@martincostello.com>
2024-07-03 19:33:27 +0100

    Remove SignalR from message processor
    Only broadcast to SignalR when receiving the HTTP payload, not when processing the message payloads.

--- src/Costellobot/GitHubMessageProcessor.cs
+++ src/Costellobot/GitHubMessageProcessor.cs
@@ -2,37 +2,21 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 using System.Text.Json;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Primitives;
 using Octokit.Webhooks;
 
 namespace MartinCostello.Costellobot;
 
 public sealed partial class GitHubMessageProcessor(
-    IHubContext<GitHubWebhookHub, IWebhookClient> hub,
     IServiceProvider serviceProvider,
     ILogger<GitHubMessageProcessor> logger) : WebhookEventProcessor
 {
-    private static readonly string[] HeadersToLog =
-    [
-        "Accept",
-        "Content-Type",
-        "User-Agent",
-        "X-GitHub-Delivery",
-        "X-GitHub-Event",
-        "X-GitHub-Hook-ID",
-        "X-GitHub-Hook-Installation-Target-ID",
-        "X-GitHub-Hook-Installation-Target-Type",
-        "X-Hub-Signature",
-        "X-Hub-Signature-256",
-    ];
-
     public override async Task ProcessWebhookAsync(IDictionary<string, StringValues> headers, string body)
     {
         ArgumentNullException.ThrowIfNull(headers);
         ArgumentNullException.ThrowIfNull(body);
 
-        (var rawHeaders, var rawPayload) = await BroadcastLogAsync(headers, body);
+        (var rawHeaders, var rawPayload) = ParseRaw(headers, body);
 
         var webhookHeaders = WebhookHeaders.Parse(headers);
         var webhookEvent = DeserializeWebhookEvent(webhookHeaders, body);
@@ -41,28 +25,23 @@ public sealed partial class GitHubMessageProcessor(
         await ProcessAsync(new(webhookHeaders, webhookEvent, rawHeaders, rawPayload));
     }
 
-    private async Task<(IDictionary<string, string> Headers, JsonElement Payload)> BroadcastLogAsync(
+    private static (IDictionary<string, string> Headers, JsonElement Payload) ParseRaw(
         IDictionary<string, StringValues> headers,
         string body)
     {
         try
         {
             // HACK Cannot serialize the parsed webhook objects as-is because DateTimeOffset does
-            // not support being serialized and throws an exception, which breaks the SignalR connection.
+            // not support being serialized and throws an exception.
             // See https://github.com/octokit/webhooks.net/blob/1a6ce29f8312c555227703057ba45723e3c78574/src/Octokit.Webhooks/Converter/DateTimeOffsetConverter.cs#L14.
             using var document = JsonDocument.Parse(body);
 
-            var webhookHeaders = new Dictionary<string, string>(HeadersToLog.Length);
+            var webhookHeaders = new Dictionary<string, string>(headers.Count);
 
-            foreach (string header in HeadersToLog)
+            foreach ((var key, var values) in headers)
             {
-                if (headers.TryGetValue(header, out var value))
-                {
-                    webhookHeaders[header] = value!;
-                }
+                webhookHeaders[key] = values.ToString();
             }
-
-            await hub.Clients.All.WebhookAsync(webhookHeaders, document.RootElement);
 
             return (webhookHeaders, document.RootElement.Clone());
         }
