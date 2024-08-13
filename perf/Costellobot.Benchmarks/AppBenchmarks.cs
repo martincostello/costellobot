75232a1c86c4b10bfe061391ea06a40d8dd497b4
martincostello <martin@martincostello.com>
2024-08-13 09:44:31 +0100

    Add continuous benchmarks
    Add GitHub Actions workflow to run continuous benchmarks and publish to a tracking website.

---
+++ perf/Costellobot.Benchmarks/AppBenchmarks.cs
@@ -0,0 +1,108 @@
+﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Net.Http.Json;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnosers;
+
+namespace MartinCostello.Costellobot.Benchmarks;
+
+[EventPipeProfiler(EventPipeProfile.CpuSampling)]
+[MemoryDiagnoser]
+public class AppBenchmarks : IAsyncDisposable
+{
+    private AppServer? _app = new();
+    private HttpClient? _client;
+    private bool _disposed;
+
+    [GlobalSetup]
+    public async Task StartServer()
+    {
+        if (_app is { } app)
+        {
+            await app.StartAsync();
+            _client = app.CreateHttpClient();
+        }
+    }
+
+    [GlobalCleanup]
+    public async Task StopServer()
+    {
+        if (_app is { } app)
+        {
+            await app.StopAsync();
+            _app = null;
+        }
+    }
+
+    [Benchmark]
+    public async Task<byte[]> Root()
+        => await _client!.GetByteArrayAsync("/");
+
+    [Benchmark]
+    public async Task<byte[]> Version()
+        => await _client!.GetByteArrayAsync("/version");
+
+    [Benchmark]
+    public async Task<byte[]> Webhook()
+    {
+        var payload = new
+        {
+            zen = "You tried your best and you failed miserably. The lesson is, never try.",
+            hook_id = 109948940,
+            hook = new
+            {
+                type = "App",
+                id = 109948940,
+                name = "web",
+                active = true,
+                events = new[] { "*" },
+            },
+            config = new
+            {
+                content_type = "json",
+                insecure_ssl = "0",
+                url = "https://costellobot.martincostello.local/github-webhook",
+            },
+            updated_at = "2022-03-23T23:13:43Z",
+            created_at = "2022-03-23T23:13:43Z",
+            app_id = 349596565,
+            deliveries_url = "https://api.github.com/app/hook/deliveries",
+            installation = new
+            {
+                id = 42,
+            },
+        };
+
+        using var message = new HttpRequestMessage(HttpMethod.Post, "/github-webhook");
+        message.Headers.Add("X-GitHub-Delivery", "0a9af448-2c9e-4fca-bcc5-4af1d1149dbc");
+        message.Headers.Add("X-GitHub-Event", "ping");
+        message.Headers.Add("X-Hub-Signature-256", "sha256=b495b976c00ff7450a4715fd0eb812ce258643990116a925688d209b1f46e353");
+        message.Content = JsonContent.Create(payload);
+
+        using var response = await _client!.SendAsync(message);
+
+        response.EnsureSuccessStatusCode();
+
+        return await response!.Content!.ReadAsByteArrayAsync();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+
+        if (!_disposed)
+        {
+            _client?.Dispose();
+            _client = null;
+
+            if (_app is not null)
+            {
+                await _app.DisposeAsync();
+                _app = null;
+            }
+        }
+
+        _disposed = true;
+    }
+}
---
+++ perf/Costellobot.Benchmarks/AppServer.cs
@@ -0,0 +1,153 @@
+﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using Azure.Messaging.ServiceBus;
+using MartinCostello.Costellobot.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace MartinCostello.Costellobot.Benchmarks;
+
+internal sealed class AppServer : IAsyncDisposable
+{
+    private WebApplication? _app;
+    private Uri? _baseAddress;
+    private bool _disposed;
+
+    public AppServer()
+    {
+        var builder = WebApplication.CreateBuilder([$"--contentRoot={GetContentRoot()}"]);
+
+        ConfigureWebHost(builder);
+
+        builder.AddCostellobot();
+
+        builder.Logging.SetMinimumLevel(LogLevel.Debug);
+
+        _app = builder.Build();
+        _app.UseCostellobot();
+    }
+
+    public HttpClient CreateHttpClient()
+    {
+        var handler = new HttpClientHandler()
+        {
+            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator,
+        };
+
+#pragma warning disable CA5400
+        return new(handler, disposeHandler: true) { BaseAddress = _baseAddress };
+#pragma warning restore CA5400
+    }
+
+    public async Task StartAsync()
+    {
+        if (_app is { } app)
+        {
+            await app.StartAsync();
+
+            var server = app.Services.GetRequiredService<IServer>();
+            var addresses = server.Features.Get<IServerAddressesFeature>();
+
+            _baseAddress = addresses!.Addresses
+                .Select((p) => new Uri(p))
+                .Last();
+        }
+    }
+
+    public async Task StopAsync()
+    {
+        if (_app is { } app)
+        {
+            await app.StopAsync();
+            _app = null;
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+
+        if (!_disposed && _app is not null)
+        {
+            await _app.DisposeAsync();
+        }
+
+        _disposed = true;
+    }
+
+    internal static void ConfigureWebHost(WebApplicationBuilder builder)
+    {
+        builder.Logging.ClearProviders();
+
+        builder.Services.AddSingleton<ServiceBusClient, InMemoryServiceBusClient>();
+
+        builder.WebHost.UseUrls("https://127.0.0.1:0");
+
+        builder.WebHost.UseSetting(
+            "ConnectionStrings:AzureServiceBus",
+            "costellobot.servicebus.windows.local");
+
+        var testKeyPath = Path.Combine(
+            GetRepositoryPath() ?? Environment.CurrentDirectory,
+            "tests",
+            "Costellobot.Tests",
+            "costellobot-tests.pem");
+
+        string testKey = File.ReadAllText(testKeyPath);
+
+        var config = new[]
+        {
+            KeyValuePair.Create<string, string?>("ConnectionStrings:AzureBlobStorage", string.Empty),
+            KeyValuePair.Create<string, string?>("ConnectionStrings:AzureKeyVault", string.Empty),
+            KeyValuePair.Create<string, string?>("GitHub:AccessToken", "gho_github-access-token"),
+            KeyValuePair.Create<string, string?>("GitHub:AppId", "123"),
+            KeyValuePair.Create<string, string?>("GitHub:BadgesKey", "badges-key"),
+            KeyValuePair.Create<string, string?>("GitHub:ClientId", "github-id"),
+            KeyValuePair.Create<string, string?>("GitHub:ClientSecret", "github-secret"),
+            KeyValuePair.Create<string, string?>("GitHub:EnterpriseDomain", string.Empty),
+            KeyValuePair.Create<string, string?>("GitHub:InstallationId", "42"),
+            KeyValuePair.Create<string, string?>("GitHub:PrivateKey", testKey),
+            KeyValuePair.Create<string, string?>("GitHub:WebhookSecret", "github-webhook-secret"),
+            KeyValuePair.Create<string, string?>("HostOptions:ShutdownTimeout", "00:00:01"),
+            KeyValuePair.Create<string, string?>("Webhook:QueueName", "github-webhooks"),
+        };
+
+        builder.Configuration.AddInMemoryCollection(config);
+    }
+
+    private static string? GetRepositoryPath()
+    {
+        var directoryInfo = new DirectoryInfo(Path.GetDirectoryName(typeof(AppServer).Assembly.Location)!);
+
+        do
+        {
+            string? solutionPath = Directory.EnumerateFiles(directoryInfo.FullName, "Costellobot.sln").FirstOrDefault();
+
+            if (solutionPath is not null)
+            {
+                return Path.GetDirectoryName(solutionPath);
+            }
+
+            directoryInfo = directoryInfo.Parent;
+        }
+        while (directoryInfo is not null);
+
+        return null;
+    }
+
+    private static string GetContentRoot()
+    {
+        if (GetRepositoryPath() is { } repoPath)
+        {
+            return Path.GetFullPath(Path.Combine(repoPath, "src", "Costellobot"));
+        }
+
+        return string.Empty;
+    }
+}
