9632090b1484138d230cdc1d0573ef240ceb974c
Martin Costello <martin@martincostello.com>
2024-08-23 19:55:30 +0100

    Remove redundant line
    Remove redundant `continue` statement.

--- src/Costellobot/GitDiffParser.cs
+++ src/Costellobot/GitDiffParser.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
+// Copyright (c) Martin Costello, 2022. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 using System.Diagnostics;
@@ -123,7 +123,6 @@ public static class GitDiffParser
             if (line.StartsWith(HunkPrefix, StringComparison.Ordinal))
             {
                 inHunk = true;
-                continue;
             }
             else if (inHunk)
             {
