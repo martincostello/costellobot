52cb5ed15aade6ee29dd401a49a919e743caea0c
martincostello <martin@martincostello.com>
2024-08-23 10:25:28 +0100

    Parse Git diffs
    Parse Git diffs to extract which NuGet packages were updated and the versions they were updated from and to.

---
+++ src/Costellobot/GitDiffParser.cs
@@ -0,0 +1,229 @@
+﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Xml.Linq;
+using NuGet.Versioning;
+
+namespace MartinCostello.Costellobot;
+
+public static class GitDiffParser
+{
+    private const char Added = '+';
+    private const char Removed = '-';
+    private const string HunkPrefix = "@@ ";
+
+    private static readonly XName Include = nameof(Include);
+    private static readonly XName GlobalPackageReference = nameof(GlobalPackageReference);
+    private static readonly XName PackageReference = nameof(PackageReference);
+    private static readonly XName PackageVersion = nameof(PackageVersion);
+    private static readonly XName Version = nameof(Version);
+
+    private enum DiffLineType
+    {
+        None = 0,
+        Added,
+        Removed,
+    }
+
+    public static bool TryParseUpdatedPackages(
+        string diff,
+        [NotNullWhen(true)] out IDictionary<string, (NuGetVersion From, NuGetVersion To)>? packages)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(diff);
+
+        packages = null;
+
+        var edits = GetEdits(diff);
+
+        if (edits.Count < 1)
+        {
+            return false;
+        }
+
+        var packageUpdates = GetPackages(edits);
+
+        if (packageUpdates.Count < 1)
+        {
+            return false;
+        }
+
+        packages = packageUpdates;
+        return true;
+    }
+
+    private static Dictionary<string, (NuGetVersion From, NuGetVersion To)> GetPackages(
+        List<(DiffLineType Type, string Package, NuGetVersion Version)> edits)
+    {
+        var comparer = StringComparer.OrdinalIgnoreCase;
+        var added = new Dictionary<string, IList<NuGetVersion>>(comparer);
+        var removed = new Dictionary<string, IList<NuGetVersion>>(comparer);
+
+        foreach (var edit in edits)
+        {
+            Dictionary<string, IList<NuGetVersion>> target;
+
+            if (edit.Type == DiffLineType.Added)
+            {
+                target = added;
+            }
+            else
+            {
+                Debug.Assert(edit.Type is DiffLineType.Removed, "Expected a DiffLineType of Removed.");
+                target = removed;
+            }
+
+            if (!target.TryGetValue(edit.Package, out var versions))
+            {
+                target[edit.Package] = versions = [];
+            }
+
+            versions.Add(edit.Version);
+        }
+
+        var packages = new Dictionary<string, (NuGetVersion From, NuGetVersion To)>(comparer);
+
+        foreach ((var package, var previous) in removed)
+        {
+            if (previous.Count < 1 ||
+                !added.TryGetValue(package, out var updated) ||
+                updated.Count < 1)
+            {
+                continue;
+            }
+
+            var minimum = previous.Min()!;
+            var maximum = updated.Max()!;
+
+            if (minimum < maximum)
+            {
+                packages[package] = (minimum, maximum);
+            }
+        }
+
+        return packages;
+    }
+
+    private static List<(DiffLineType Type, string Package, NuGetVersion Version)> GetEdits(string diff)
+    {
+        using var reader = new StringReader(diff);
+        string? line;
+
+        bool inHunk = false;
+
+        var edits = new List<(DiffLineType Type, string Package, NuGetVersion Version)>();
+
+        while ((line = reader.ReadLine()) is not null)
+        {
+            if (line.StartsWith(HunkPrefix, StringComparison.Ordinal))
+            {
+                inHunk = true;
+                continue;
+            }
+            else if (inHunk)
+            {
+                char prefix = line[0];
+
+                if (prefix is Added or Removed)
+                {
+                    if (TryParseLine(line, out var type, out var package))
+                    {
+                        edits.Add((type, package.Value.Package, package.Value.Version));
+                    }
+
+                    continue;
+                }
+                else if (prefix is ' ')
+                {
+                    continue;
+                }
+
+                inHunk = false;
+            }
+        }
+
+        return edits;
+    }
+
+    private static bool TryParseLine(
+        string line,
+        out DiffLineType lineType,
+        [NotNullWhen(true)] out (string Package, NuGetVersion Version)? package)
+    {
+        lineType = DiffLineType.None;
+        package = null;
+
+        if (string.IsNullOrWhiteSpace(line))
+        {
+            return false;
+        }
+
+        lineType = line[0] switch
+        {
+            Added => DiffLineType.Added,
+            Removed => DiffLineType.Removed,
+            _ => DiffLineType.None,
+        };
+
+        if (lineType == DiffLineType.None)
+        {
+            return false;
+        }
+
+        var fragmentText = line[1..];
+
+        if (!fragmentText.Contains('<', StringComparison.Ordinal))
+        {
+            return false;
+        }
+
+        XElement fragment;
+
+        try
+        {
+            fragment = XElement.Parse(fragmentText);
+
+            if (fragment.Name == PackageReference ||
+                fragment.Name == PackageVersion ||
+                fragment.Name == GlobalPackageReference)
+            {
+                if (TryParsePackage(fragment, out var name, out var version))
+                {
+                    package = (name, version);
+                    return true;
+                }
+            }
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+
+        return false;
+
+        static bool TryParsePackage(
+            XElement element,
+            [NotNullWhen(true)] out string? name,
+            [NotNullWhen(true)] out NuGetVersion? version)
+        {
+            name = null;
+            version = null;
+
+            if (element.Attribute(Include) is not { } include ||
+                include.Value is not { Length: > 0 } packageId)
+            {
+                return false;
+            }
+
+            if (element.Attribute(Version) is not { } attribute ||
+                attribute.Value is not { Length: > 0 } versionString)
+            {
+                return false;
+            }
+
+            name = packageId;
+            return NuGetVersion.TryParse(versionString, out version);
+        }
+    }
+}
---
+++ tests/Costellobot.Tests/GitDiffParserTests.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.Costellobot;
+
+public static class GitDiffParserTests
+{
+    [Fact]
+    public static void TryParseUpdatedPackages_Parses_Diff_Correctly_For_One_File()
+    {
+        // Arrange
+        string diff = GetDiff("OneFile");
+
+        // Act
+        bool actual = GitDiffParser.TryParseUpdatedPackages(diff, out var packages);
+
+        // Assert
+        actual.ShouldBeTrue();
+        packages.ShouldNotBeNull();
+        packages.ShouldContainKeyAndValue("AWSSDK.SecurityToken", (new("3.7.400.10"), new("3.7.400.11")));
+        packages.ShouldContainKeyAndValue("AWSSDK.SimpleSystemsManagement", (new("3.7.401.8"), new("3.7.401.9")));
+        packages.Count.ShouldBe(2);
+    }
+
+    [Fact]
+    public static void TryParseUpdatedPackages_Parses_Diff_Correctly_For_Multiple_Files()
+    {
+        // Arrange
+        string diff = GetDiff("MultipleFiles");
+
+        // Act
+        bool actual = GitDiffParser.TryParseUpdatedPackages(diff, out var packages);
+
+        // Assert
+        actual.ShouldBeTrue();
+        packages.ShouldNotBeNull();
+        packages.ShouldContainKeyAndValue("Microsoft.AspNetCore.Mvc.Testing", (new("8.0.8"), new("9.0.0-preview.7.24406.2")));
+        packages.ShouldContainKeyAndValue("Microsoft.EntityFrameworkCore.Sqlite", (new("8.0.8"), new("9.0.0-preview.7.24405.3")));
+        packages.Count.ShouldBe(2);
+    }
+
+    [Fact]
+    public static void TryParseUpdatedPackages_Parses_Diff_Correctly_For_No_Package_Updates()
+    {
+        // Arrange
+        string diff = GetDiff("NoUpdates");
+
+        // Act
+        bool actual = GitDiffParser.TryParseUpdatedPackages(diff, out var packages);
+
+        // Assert
+        actual.ShouldBeFalse();
+        packages.ShouldBeNull();
+    }
+
+    private static string GetDiff(string name)
+    {
+        var type = typeof(GitDiffParserTests);
+        var assembly = type.Assembly;
+        var resource = assembly.GetManifestResourceStream($"{type.Namespace}.GitDiffParserTests.{name}.diff")!;
+        using var reader = new StreamReader(resource);
+        return reader.ReadToEnd();
+    }
+}
