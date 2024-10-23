2aa52405dd9dbe987fe8177e482fa2656090067f
martincostello <martin@martincostello.com>
2024-10-23 11:06:13 +0100

    Publish fewer messages to ServiceBus
    Reduce the number of GitHub Webhook payloads actually published to Azure ServiceBus by ignoring events not in the list of known payloads.

---
+++ src/Costellobot/WellKnownGitHubEvents.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using Octokit.Webhooks;
+using Octokit.Webhooks.Events.CheckSuite;
+using Octokit.Webhooks.Events.DeploymentProtectionRule;
+using Octokit.Webhooks.Events.DeploymentStatus;
+using Octokit.Webhooks.Events.IssueComment;
+using Octokit.Webhooks.Events.PullRequest;
+
+namespace MartinCostello.Costellobot;
+
+/// <summary>
+/// Defines all of the known/consumed GitHub webhook events.
+/// </summary>
+public static class WellKnownGitHubEvents
+{
+    private static readonly HashSet<(string? Event, string? Action)> KnownEvents =
+    [
+        (WebhookEventType.CheckSuite, CheckSuiteAction.Completed),
+        (WebhookEventType.DeploymentProtectionRule, DeploymentProtectionRuleAction.Requested),
+        (WebhookEventType.DeploymentStatus, DeploymentStatusActionValue.Created),
+        (WebhookEventType.IssueComment, IssueCommentActionValue.Created),
+        (WebhookEventType.Ping, null),
+        (WebhookEventType.Push, null),
+        (WebhookEventType.PullRequest, PullRequestActionValue.Labeled),
+        (WebhookEventType.PullRequest, PullRequestActionValue.Opened),
+    ];
+
+    public static bool IsKnown(GitHubEvent message)
+        => KnownEvents.Contains((message.Headers.Event, message.Event.Action));
+}
