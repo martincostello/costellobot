bf07c590ea311a4f27d1c8cabf53789c70f68558
martincostello <martin@martincostello.com>
2024-10-23 11:06:13 +0100

    Publish fewer messages to ServiceBus
    Reduce the number of GitHub Webhook payloads actually published to Azure ServiceBus by ignoring events not in the list of known payloads.

--- src/Costellobot/GitHubEventHandler.cs
+++ src/Costellobot/GitHubEventHandler.cs
@@ -20,10 +20,17 @@ public sealed partial class GitHubEventHandler(
 
         try
         {
-            var message = GitHubMessageSerializer.Serialize(payload.Headers.Delivery, payload.RawHeaders, payload.RawPayload.ToString());
+            if (WellKnownGitHubEvents.IsKnown(payload))
+            {
+                var message = GitHubMessageSerializer.Serialize(payload.Headers.Delivery, payload.RawHeaders, payload.RawPayload.ToString());
 
-            var sender = client.CreateSender(config.QueueName);
-            await sender.SendMessageAsync(message, cts.Token);
+                var sender = client.CreateSender(config.QueueName);
+                await sender.SendMessageAsync(message, cts.Token);
+            }
+            else
+            {
+                Log.IgnoringEvent(logger, payload.Headers.Delivery, payload.Headers.Event, payload.Event.Action);
+            }
         }
         catch (Exception ex)
         {
@@ -42,5 +49,11 @@ public sealed partial class GitHubEventHandler(
            Level = LogLevel.Error,
            Message = "Failed to publish message for webhook with ID {HookId}.")]
         public static partial void PublishFailed(ILogger logger, Exception exception, string? hookId);
+
+        [LoggerMessage(
+           EventId = 2,
+           Level = LogLevel.Debug,
+           Message = "Ignoring GitHub webhook with ID {HookId} for event {Event}:{Action}.")]
+        public static partial void IgnoringEvent(ILogger logger, string? hookId, string? @event, string? action);
     }
 }
