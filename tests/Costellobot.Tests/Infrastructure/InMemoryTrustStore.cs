71cbb0a78257b8d6b19af0db392d8419352f5fef
martincostello <martin@martincostello.com>
2025-02-18 15:38:37 +0000

    Fix mock
    Fix missing method.

--- tests/Costellobot.Tests/Infrastructure/InMemoryTrustStore.cs
+++ tests/Costellobot.Tests/Infrastructure/InMemoryTrustStore.cs
@@ -7,6 +7,16 @@ internal sealed class InMemoryTrustStore : ITrustStore
 {
     private readonly HashSet<(DependencyEcosystem Ecosystem, string Id, string Version)> _trustStore = [];
 
+    public Task<IReadOnlyList<KeyValuePair<string, string>>> GetTrustAsync(DependencyEcosystem ecosystem, CancellationToken cancellationToken = default)
+    {
+        var trusted = _trustStore
+            .Where((p) => p.Ecosystem == ecosystem)
+            .Select((p) => KeyValuePair.Create(p.Id, p.Version))
+            .ToList();
+
+        return Task.FromResult<IReadOnlyList<KeyValuePair<string, string>>>(trusted);
+    }
+
     public Task<bool> IsTrustedAsync(DependencyEcosystem ecosystem, string id, string version, CancellationToken cancellationToken = default)
     {
         bool isTrusted = _trustStore.Contains((ecosystem, id, version));
