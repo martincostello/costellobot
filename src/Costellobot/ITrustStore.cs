0133ee0b261d72ae2b78c3ddd57b400e604ec6a6
martincostello <martin@martincostello.com>
2025-02-22 14:40:26 +0000

    Update trust store model
    Replace key-value pairs with a `TrustedDependency` model.

--- src/Costellobot/ITrustStore.cs
+++ src/Costellobot/ITrustStore.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using MartinCostello.Costellobot.Models;
+
 namespace MartinCostello.Costellobot;
 
 public interface ITrustStore
@@ -11,7 +13,7 @@ public interface ITrustStore
         string version,
         CancellationToken cancellationToken = default);
 
-    Task<IReadOnlyList<KeyValuePair<string, string>>> GetTrustAsync(
+    Task<IReadOnlyList<TrustedDependency>> GetTrustAsync(
        DependencyEcosystem ecosystem,
        CancellationToken cancellationToken = default);
 
---
+++ src/Costellobot/Models/TrustedDependency.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.Costellobot.Models;
+
+public sealed record TrustedDependency(
+    string Id,
+    string Version)
+{
+    public DateTimeOffset? TrustedAt { get; set; }
+}
