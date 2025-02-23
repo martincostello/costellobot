af7881df9789e4abc0f5769654b13b4691b12806
martincostello <martin@martincostello.com>
2025-02-23 13:45:00 +0000

    Use concurrent dictionary
    Guard against any concurrency issues with the in-memory trust store.

--- tests/Costellobot.Tests/Infrastructure/InMemoryTrustStore.cs
+++ tests/Costellobot.Tests/Infrastructure/InMemoryTrustStore.cs
@@ -1,25 +1,26 @@
 ﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using System.Collections.Concurrent;
 using MartinCostello.Costellobot.Models;
 
 namespace MartinCostello.Costellobot.Infrastructure;
 
 internal sealed class InMemoryTrustStore : ITrustStore
 {
-    private readonly HashSet<(DependencyEcosystem Ecosystem, string Id, string Version)> _trustStore = [];
+    private readonly ConcurrentDictionary<(DependencyEcosystem Ecosystem, string Id, string Version), bool> _trustStore = new();
 
     public int Count => _trustStore.Count;
 
     public Task DistrustAsync(DependencyEcosystem ecosystem, string id, string version, CancellationToken cancellationToken = default)
     {
-        _trustStore.Remove((ecosystem, id, version));
+        _trustStore.Remove((ecosystem, id, version), out _);
         return Task.CompletedTask;
     }
 
     public Task<IReadOnlyList<TrustedDependency>> GetTrustAsync(DependencyEcosystem ecosystem, CancellationToken cancellationToken = default)
     {
-        var trusted = _trustStore
+        var trusted = _trustStore.Keys
             .Where((p) => p.Ecosystem == ecosystem)
             .Select((p) => new TrustedDependency(p.Id, p.Version))
             .ToList();
@@ -29,13 +30,13 @@ internal sealed class InMemoryTrustStore : ITrustStore
 
     public Task<bool> IsTrustedAsync(DependencyEcosystem ecosystem, string id, string version, CancellationToken cancellationToken = default)
     {
-        bool isTrusted = _trustStore.Contains((ecosystem, id, version));
+        bool isTrusted = _trustStore.ContainsKey((ecosystem, id, version));
         return Task.FromResult(isTrusted);
     }
 
     public Task TrustAsync(DependencyEcosystem ecosystem, string id, string version, CancellationToken cancellationToken = default)
     {
-        _trustStore.Add((ecosystem, id, version));
+        _trustStore[(ecosystem, id, version)] = true;
         return Task.CompletedTask;
     }
 
