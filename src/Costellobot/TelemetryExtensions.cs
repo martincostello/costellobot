a45518f589ab72e1aaa7348dccb835925fe0a258
martincostello <martin@martincostello.com>
2025-03-16 16:33:24 +0000

    Remove Azure Monitor
    Remove references to Azure Monitor.

--- src/Costellobot/TelemetryExtensions.cs
+++ src/Costellobot/TelemetryExtensions.cs
@@ -4,7 +4,6 @@
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using AspNet.Security.OAuth.GitHub;
-using Azure.Monitor.OpenTelemetry.Exporter;
 using Microsoft.Extensions.Options;
 using OpenTelemetry.Instrumentation.AspNetCore;
 using OpenTelemetry.Instrumentation.Http;
@@ -34,12 +33,6 @@ public static class TelemetryExtensions
     {
         ArgumentNullException.ThrowIfNull(services);
 
-        if (IsAzureMonitorConfigured())
-        {
-            services.Configure<AzureMonitorExporterOptions>(
-                (p) => p.ConnectionString = AzureMonitorConnectionString());
-        }
-
         services
             .AddOpenTelemetry()
             .WithMetrics((builder) =>
@@ -49,11 +42,6 @@ public static class TelemetryExtensions
                        .AddHttpClientInstrumentation()
                        .AddProcessInstrumentation()
                        .AddRuntimeInstrumentation();
-
-                if (IsAzureMonitorConfigured())
-                {
-                    builder.AddAzureMonitorMetricExporter();
-                }
 
                 if (IsOtlpCollectorConfigured())
                 {
@@ -75,11 +63,6 @@ public static class TelemetryExtensions
                 if (environment.IsDevelopment())
                 {
                     builder.SetSampler(new AlwaysOnSampler());
-                }
-
-                if (IsAzureMonitorConfigured())
-                {
-                    builder.AddAzureMonitorTraceExporter();
                 }
 
                 if (IsOtlpCollectorConfigured())
@@ -119,12 +102,6 @@ public static class TelemetryExtensions
 
     internal static bool IsOtlpCollectorConfigured()
         => !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT"));
-
-    internal static bool IsAzureMonitorConfigured()
-        => !string.IsNullOrEmpty(AzureMonitorConnectionString());
-
-    private static string? AzureMonitorConnectionString()
-        => Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING");
 
     private static void EnrichHttpActivity(Activity activity, HttpRequestMessage request)
     {
