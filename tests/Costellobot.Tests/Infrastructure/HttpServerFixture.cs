9bab98d2c6163dd8305f944b77521002d9a34f7f
martincostello <martin@martincostello.com>
2025-07-10 14:17:16 +0100

    Add missing using
    Add missing using for `IPAddress`.

--- tests/Costellobot.Tests/Infrastructure/HttpServerFixture.cs
+++ tests/Costellobot.Tests/Infrastructure/HttpServerFixture.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using System.Net;
 using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Hosting;
