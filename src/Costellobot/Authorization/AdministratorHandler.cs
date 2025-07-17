8521e427757241d14a3137eea3ec935a119234ca
martincostello <martin@martincostello.com>
2025-07-17 09:55:58 +0100

    Refactor away duplication
    Refactor authorization handlers to not duplicate logic.

--- src/Costellobot/Authorization/AdministratorHandler.cs
+++ src/Costellobot/Authorization/AdministratorHandler.cs
@@ -1,75 +1,20 @@
 ﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Options;
 
 namespace MartinCostello.Costellobot.Authorization;
 
-public sealed partial class AdministratorHandler(IOptions<SiteOptions> options) : AuthorizationHandler<AdministratorRequirement>
+public sealed partial class AdministratorHandler(IOptionsMonitor<SiteOptions> options) : AuthorizationHandler<AdministratorRequirement>
 {
     protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, AdministratorRequirement requirement)
     {
-        if (IsAuthorized(context.User, options.Value))
+        if (context.User.IsAdministrator(options.CurrentValue))
         {
             context.Succeed(requirement);
         }
 
         return Task.CompletedTask;
-    }
-
-    private static bool IsAuthorized(ClaimsPrincipal user, SiteOptions options)
-    {
-        bool hasClaim = false;
-        bool needsClaim = false;
-
-        if (options.AdminUsers is { Count: > 0 } users)
-        {
-            needsClaim = true;
-
-            foreach (var name in users)
-            {
-                if (user.HasClaim(ClaimTypes.Name, name))
-                {
-                    hasClaim = true;
-                    break;
-                }
-            }
-        }
-
-        bool hasRole = false;
-        bool needsRole = false;
-
-        if (options.AdminRoles is { Count: > 0 } roles)
-        {
-            needsRole = true;
-
-            foreach (var role in roles)
-            {
-                if (user.IsInRole(role))
-                {
-                    hasRole = true;
-                    break;
-                }
-            }
-        }
-
-        bool authorized = false;
-
-        if (needsClaim && needsRole)
-        {
-            authorized = hasClaim && hasRole;
-        }
-        else if (needsClaim || hasClaim)
-        {
-            authorized = (needsClaim && hasClaim) || (needsRole && hasRole);
-        }
-        else if (!needsClaim && !needsRole)
-        {
-            authorized = true;
-        }
-
-        return authorized;
     }
 }
---
+++ src/Costellobot/Authorization/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Security.Claims;
+
+namespace MartinCostello.Costellobot.Authorization;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static bool IsAdministrator(this ClaimsPrincipal user, SiteOptions options)
+        => user.IsAdministrator(options.AdminRoles, options.AdminUsers);
+
+    public static bool IsAdministrator(this ClaimsPrincipal user, ICollection<string> roles, ICollection<string> users)
+    {
+        bool hasClaim = false;
+        bool needsClaim = false;
+
+        if (users is { Count: > 0 })
+        {
+            needsClaim = true;
+
+            foreach (var name in users)
+            {
+                if (user.HasClaim(ClaimTypes.Name, name))
+                {
+                    hasClaim = true;
+                    break;
+                }
+            }
+        }
+
+        bool hasRole = false;
+        bool needsRole = false;
+
+        if (roles is { Count: > 0 })
+        {
+            needsRole = true;
+
+            foreach (var role in roles)
+            {
+                if (user.IsInRole(role))
+                {
+                    hasRole = true;
+                    break;
+                }
+            }
+        }
+
+        bool authorized = false;
+
+        if (needsClaim && needsRole)
+        {
+            authorized = hasClaim && hasRole;
+        }
+        else if (needsClaim || hasClaim)
+        {
+            authorized = (needsClaim && hasClaim) || (needsRole && hasRole);
+        }
+        else if (!needsClaim && !needsRole)
+        {
+            authorized = true;
+        }
+
+        return authorized;
+    }
+}
