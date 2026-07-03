324f7df9ed1f841e14e0a2c1b4d393301035ec4f
Martin Costello <martin@martincostello.com>
2026-07-03 15:27:47 +0100

    Bump BenchmarkDotNet to 0.16.0-preview.1 (#3665)
    Update to BenchmarkDotNet 0.16.0-preview.1.

--- benchmark.ps1
+++ benchmark.ps1
@@ -62,7 +62,9 @@ $benchmarks = (Join-Path $solutionPath "perf" "Costellobot.Benchmarks" "Costello
 
 Write-Information "Running benchmarks..."
 
-$additionalArgs = @()
+$additionalArgs = @(
+    "--consumeTasksSynchronously" # For backwards compatibility with BenchmarkDotNet versions before 0.16.0
+)
 
 if (-Not [string]::IsNullOrEmpty($Filter)) {
     $additionalArgs += "--filter"
